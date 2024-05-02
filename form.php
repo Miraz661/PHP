d78dd880dbe2be72dc99a07dffcf4001a33a5a9b
miraz <105309402+Miraz661@users.noreply.github.com>
2024-05-02 10:47:08 +0600

    Get user data and manipulate them in php page

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP form</title>
+</head>
+<body>
+    <form action="get_date.php" method="POST" >
+        <label for="start">Start date:</label>
+        <input type="date" name="startDate" id="start">
+        <input type="submit" value="Submit" name="submit">
+    </form>
+</body>
+</html>
